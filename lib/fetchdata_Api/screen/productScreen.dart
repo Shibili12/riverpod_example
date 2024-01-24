7d8de16e48c408d3cfeaaea904fed708d7cf5140
shibili12 <shibiliazwin23@gmail.com>
2024-01-24 16:44:10 +0530

    commit

--- lib/fetchdata_Api/screen/productScreen.dart
+++ lib/fetchdata_Api/screen/productScreen.dart
@@ -3,22 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_example/fetchdata_Api/model/product_model.dart';
 import 'package:riverpod_example/fetchdata_Api/model/productprovider_model.dart';
 
-void main() {
-  runApp(ProviderScope(child: const MyApp()));
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ProductScreen(),
-    );
-  }
-}
-
 class ProductScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
